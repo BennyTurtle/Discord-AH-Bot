d5d5d48b131e717e31ed38ce75986aaf48d01c2c
Ben <geotraxben@gmail.com>
2019-07-23 19:48:03 -0400

    Update appmain.py

--- appmain.py
+++ appmain.py
@@ -9,9 +9,6 @@ from discord.ext import commands
 URL = "https://linode.ghazlawl.com/ark/mods/auctionhouse/api/json/v1/auctions/"
 
 
-#KEY = ""
-#ServerBase = "https://ark-servers.net/api/?object=servers&element=detail&key={}"
-#Server = ServerBase.format(KEY)
 
 bot = commands.Bot(activity=discord.Game("Ark: Auction House Mod") ,command_prefix='ah!')
 
@@ -43,7 +40,6 @@ async def botHelp(ctx):
 	await ctx.send("Commands:")
 	await ctx.send("getListing (Item Name):: Gets all the listings for that item type")
 	await ctx.send("WARNING: getListing gets every listing from every server, so please dont use it in public chat")
-	await ctx.send("online :: displays the status of the servers")
 	await ctx.send("botHelp :: displays this help page")
 	await asyncio.sleep(3)
 
